503b667a35ed6b6dd80d49ae91c52d62bc91b7ce
geek-3340 <crew.sento0864@gmail.com>
2025-06-24 00:48:34 +0900

    バリデーション機能を追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,14 +16,21 @@ class PostController extends Controller
     // storeメソッド
     // 呼び出し元ルートのリクエストデータを引数に設定
     public function store(Request $request){
+        $validated = $request->validate([
+            'title'=> 'required|max:20',
+            'body'=> 'required|max:400',
+        ]);
+
+        $post = Post::create([$validated]);
+
         // リクエストデータからtitleとbodyを取得し、Postモデルのcreateメソッド（ Post::create([]); ）を
         // 使用して新しいレコードを作成、同時にでPostモデルを介してレコードをDBサーバーに保存します。
         // 作成したレコードを$post変数に格納します。
         // 保存したレコード（モデルインスタンス）を$post変数に格納することは、後の処理で使用するためのものです。
-        $post = Post::create([
-            'title' => $request->title,
-            'body' => $request->body
-        ]);
+        // $post = Post::create([
+        //     'title' => $request->title,
+        //     'body' => $request->body
+        // ]);
         // 保存が完了したら、元のページにリダイレクトします
         // リダイレクト時に、'message'というセッション変数に
         // '保存しました'というメッセージを設定します。
